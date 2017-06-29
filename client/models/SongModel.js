aa3c79ffcc57ce12cd1b106b9e6f814dd70c3ded
Kevin Coyner <rustybear@gmail.com>
2017-06-29 16:20:13 -0400

    doodle: Pass all SongModel tests.

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,21 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function() {
+    // adds a song to the SongQueue collection
+    this.trigger('enqueue', this);
+  },
+
+  dequeue: function() {
+    // removes a song from the SongQueue collection
+    this.trigger('dequeue', this);
+  },
+
+  ended: function() {
+    // triggers an ended event
+    this.trigger('ended', this);
   }
 
 });
