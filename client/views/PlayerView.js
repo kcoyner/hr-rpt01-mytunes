605311c4c530d223f64ef7cdd228e860b4204629
Kevin Coyner <rustybear@gmail.com>
2017-07-01 17:29:36 -0400

    PlayerView complete

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -6,17 +6,29 @@ var PlayerView = Backbone.View.extend({
   el: '<audio controls autoplay />',
 
   initialize: function() {
+    var context = this;  //
     // when song ends, assign new song to play
-    this.on("ended", function() {
+    this.$el.on("ended", function() {
+      context.model.ended();
+      //console.log(this.model);
+      //console.log("this is runninggg")
+      //context.model.trigger('change:currentSong');
+      //console.log(this.model);
       //remove song that ended songQueue.at(0)
       //songQueue.at(0)
       //this.add(this.collection.at(0));
-      console.log(this.collection.at(0));
-      console.log(this);
-      setSong(this.collection.at(0));
-      // if (this.length !== 0) {
-      //   //run playFirst
-      //   this.playFirst();
+
+      // console.log(this);
+      //console.log(context);
+      //console.log(this.model.dequeue);
+      //var queue = context.model.get('songQueue')
+      //console.log(context.model.get('songQueue'));
+      //console.log(queue.at(0));
+      // context.setSong(this.model);
+      //setSong(this.collection.at(0));
+      // if (context.length !== 0) {
+      //   console.log(context)
+      //    // this.model.playFirst();
       // }
     });
     this.render();
